2af303d4a5d77e7d427b6dc3ee275b19f5fc98cf
archit2603 <archit_1901mm09@iitp.ac.in>
2020-11-03 11:15:13 +0530

    added routers

---
+++ src/routers/blog.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const Blog = require('../models/blog')
+const router = new express.Router()
+const _ = require('lodash')
+
+let posts = []
+
+router.get('/home', async (req, res) => {
+
+    try{
+        posts = await Blog.find({})
+        await res.render('home', {
+            title: 'Blogs',
+            name: 'Not Anyone',
+            posts: posts
+        })
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.get('/compose', async (req, res) => {
+    try {
+        await res.render('compose', {
+            title: 'Blogging',
+            name: 'Not Anyone'
+        })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.post('/compose', async (req, res) => {
+    const blog = new Blog({
+        'title': req.body.postTitle,
+        'body': req.body.postBody
+    })
+
+    try {
+        await blog.save()
+        posts = await Blog.find({})
+        res.status(201).redirect('/home')
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/blogs/:blogName', async (req, res) => {
+    const requestedTitle = _.lowerCase(req.params.blogName)
+
+    try {
+        posts.forEach(async (blog) => {
+            const storedTitle = _.lowerCase(blog.title)
+
+            try {
+                if (storedTitle === requestedTitle) {
+                    await res.render('post', {
+                        title: blog.title,
+                        body: blog.body,
+                        name: 'Not Anyone'
+                    })
+                }
+            } catch (e) {
+                res.status(500).send(e)
+            }
+        })
+    } catch (e) {
+        res.status(404).send(e)
+    }
+})
+
+module.exports = router
