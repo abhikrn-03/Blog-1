97ea3f77de2be294e081dd5abb1c60109da5b8c8
abhikrn-03 <abhik333333@gmail.com>
2020-11-03 08:55:14 +0530

    Auth started

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const express = require("express")
 const bodyParser = require("body-parser")
-// const hbs = require("hbs")
 const ejs = require('ejs')
 require('./db/mongoose')
 const _ = require("lodash")
 const Blog = require('./models/blog')
+const User = require('./models/user')
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,13 +15,8 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 const publicDirectoryPath = path.join(__dirname, '../public')
-// const viewsPath = path.join(__dirname, '../templates/views')
-// const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'ejs')
-// app.set('view engine', 'hbs')
-// app.set('views', 'viewsPath')
-// hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
@@ -64,6 +60,45 @@ app.get('/compose', async (req, res) => {
     }
 })
 
+app.get('/users/login', async (req, res) => {
+    
+    try {
+        await res.render('login', {
+        title: 'About',
+        name: 'Not Anyone'  
+    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+app.post("/users/signUp", async (req, res) => {
+  const user = new User({
+        "name": req.body.userName,
+        "email": req.body.userEmail,
+        "password": req.body.userPassword,
+        "gender": req.body.userGender,
+        "penName": req.body.userPen,
+        "age": req.body.userAge
+    })
+
+  try {
+        await user.save()
+        res.status(201).redirect("/home")
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.post('/users/signIn', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.loginEmail, req.body.loginPassword)
+        res.redirect("/home")
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
 app.get('/about', async (req, res) => {
     
     try {
@@ -101,7 +136,6 @@ app.post("/compose", async (req, res) => {
     } catch (e) {
         res.status(400).send(e)
     }
-
 })
 
 app.get("/blogs/:blogName", async (req, res) => {
